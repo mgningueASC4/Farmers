e754fdeec924b81148698782aae0bcb89b6d51f5
mgningueASC4 <gninguem@bxscience.edu>
2019-09-14 20:27:33 -0400

    Your good

---
+++ script.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function () {
+   // Data Picker Initialization
+   // $('.datepicker').pickadate();
+   var title;
+   var link;
+   var linkLabel;
+   var fromDate;
+   var atFromTime;
+   var atDate;
+   var atToTime;
+   var description;
+   var address;
+
+   $(".Submit").click(function () {
+      title = $('.eventTitle').val();
+      link = $('.eventLink').val()
+      linkLabel = $('.linkLabel').val()
+      fromDate = $('.fromDate').val()
+      atFromTime = $('.atFromTime').val()
+      atDate = $('.toDate').val();
+      atToTime = $('atToTime').val();
+      description = $('.description').val()
+      address = $('.address').val();
+   })
+
+});
